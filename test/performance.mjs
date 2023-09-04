4286a0998d39b445d722385066ecb8ad4800233f
Lars Hvam <larshp@hotmail.com>
2023-09-04 18:45:35 +0200

    perf setup

---
+++ test/performance.mjs
@@ -0,0 +1,17 @@
+import { initializeABAP } from '../output/init.mjs';
+import * as fs from "node:fs";
+import * as path from "node:path";
+
+await initializeABAP();
+
+for (const filename of fs.readdirSync("./test/performance")) {
+  global.gc();
+  const className = path.basename(filename.split(".")[0]).toUpperCase();
+
+  const t0 = performance.now();
+  await abap.Classes[className].run();
+  const t1 = performance.now();
+  const runtime = Math.round(t1 - t0);
+
+  console.log(className + ": " + runtime + "ms");
+}
