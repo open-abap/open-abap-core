3acc460cb9e9abed35b6aa6e854012b131210bcc
Lars Hvam <larshp@hotmail.com>
2023-09-05 07:26:40 +0200

    test

--- test/performance.mjs
+++ test/performance.mjs
@@ -4,6 +4,8 @@ import * as path from "node:path";
 
 await initializeABAP();
 
+const results = {};
+
 for (const filename of fs.readdirSync("./test/performance")) {
   global.gc();
   const className = path.basename(filename.split(".")[0]).toUpperCase();
@@ -14,4 +16,7 @@ for (const filename of fs.readdirSync("./test/performance")) {
   const runtime = Math.round(t1 - t0);
 
   console.log(className + ": " + runtime + "ms");
+  results[className] = runtime;
 }
+
+fs.writeFileSync("results.json", JSON.stringify(results, null, 2));
