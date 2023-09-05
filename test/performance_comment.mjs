25a82bdd1e926926f96f1c7e1073b553bcf48c91
Lars Hvam <larshp@hotmail.com>
2023-09-05 07:59:01 +0200

    test

--- test/performance_comment.mjs
+++ test/performance_comment.mjs
@@ -1,6 +1,6 @@
 import * as fs from "node:fs";
 
-fs.writeFileSync("results_before.json", JSON.stringify(results, null, 2));
-fs.writeFileSync("results_after.json", JSON.stringify(results, null, 2));
+const before = JSON.parse(fs.readFileSync("results_before.json"));
+const after = JSON.parse(fs.writeFileSync("results_after.json"));
 
 fs.writeFileSync("body.txt", "Performance test results: sdf");
