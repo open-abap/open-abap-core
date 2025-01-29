22567333d8586f07c3f8ac2cbc174a9cc9a07039
Lars HVam <larshp@hotmail.com>
2025-01-29 08:26:44 +0100

    init pg

---
+++ test/adbc/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE films (
+    code        char(5),
+    title       varchar(40),
+    did         integer,
+    date_prod   date,
+    kind        varchar(10),
+    len         interval hour to minute,
+    CONSTRAINT code_title PRIMARY KEY(code,title)
+);
